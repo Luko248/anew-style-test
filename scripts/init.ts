f3f673c51507343f505445283ebdf1e4cf70d592
lchylik <lukas.chylik@ais-servis.cz>
2023-12-10 13:46:13 +0100

    add header video

--- scripts/init.ts
+++ scripts/init.ts
@@ -2,11 +2,11 @@ import { initBeforeAfterSlider } from "./_beforeAfterSlider"
 import { detectAvifSupport } from "./_detectAvifSupport"
 import { initFancyBox } from "./_fancyBox"
 import { initNav } from "./_nav"
-import { initSplide } from "./_splitejs"
+// import { initSplide } from "./_splitejs"
 
 document.addEventListener("DOMContentLoaded", () => {
   detectAvifSupport()
-  initSplide()
+  // initSplide()
   initBeforeAfterSlider()
   initFancyBox()
   initNav()
